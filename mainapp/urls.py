57a2616c632ec569d7605073e2bca649323f3d6a
Tolik <99863916+AnatoliiSyrchin@users.noreply.github.com>
2023-02-11 00:12:33 +0300

    Lesson 7 (#3)
    * lesson_7 Logging, cache, celery

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -7,11 +8,14 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
-    path("news/", views.NewsListView.as_view(), name="news"),
+    path(
+        "news/",
+        cache_page(60 * 5)(views.NewsListView.as_view()),
+        name="news"),
     path("news/create/", views.NewsCreateView.as_view(), name="news_create"),
     path(
         "news/<int:pk>/detail",
-        views.NewsDetailView.as_view(),
+        cache_page(60 * 5)(views.NewsDetailView.as_view()),
         name="news_detail",
     ),
     path(
@@ -27,7 +31,7 @@ urlpatterns = [
     path("courses/", views.CoursesListView.as_view(), name="courses"),
     path(
         "courses/<int:pk>/",
-        views.CoursesDetailView.as_view(),
+        cache_page(60 * 5)(views.CoursesDetailView.as_view()),
         name="courses_detail",
     ),
     path(
@@ -37,4 +41,7 @@ urlpatterns = [
     ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("log_view/", views.LogView.as_view(), name="log_view"),
+    path("log_view_last/", views.LogViewLast.as_view(), name="log_view_last"),
+    path("log_download/", views.LogDownloadView.as_view(), name="log_download"),
 ]
