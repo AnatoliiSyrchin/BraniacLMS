2ee8056651d89b9f92217a7cf3b96d9013e9bee7
Tolik <9779930@mail.ru>
2023-02-12 19:48:19 +0300

    lesson_8 Tests

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,4 @@
 import logging
-import subprocess
 
 from django.conf import settings
 from django.contrib import messages
@@ -86,7 +85,7 @@ class CoursesDetailView(TemplateView):
         if not cached_feedback:
             context["feedback_list"] = (
                 mainapp_models.CourseFeedback.objects.filter(course=context["course_object"])
-                .order_by("-created", "-rating")
+                .order_by("-created", "-rating")[:5]
                 .select_related()
             )
             cache.set(f"feedback_list_{pk}", context["feedback_list"], timeout=300)  # 5 minutes
@@ -95,7 +94,6 @@ class CoursesDetailView(TemplateView):
             import pickle
 
             with open(f"mainapp/fixtures/005_feedback_list_{pk}.bin", "wb") as outf:
-                # print('feedback_List', context["feedback_list"])
                 pickle.dump(context["feedback_list"], outf)
             # <--- Archive object for tests
 
@@ -165,16 +163,6 @@ class LogView(TemplateView):
                     break
                 log_slice.insert(0, line)  # append at start
             context["log"] = "".join(log_slice)
-        return context
-
-
-class LogViewLast(TemplateView):
-    template_name = "mainapp/log_view_last.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(LogViewLast, self).get_context_data(**kwargs)
-        log_tail = subprocess.check_output(["tail", "-n 1000", settings.LOG_FILE], universal_newlines=True)
-        context["log"] = log_tail
         return context
 
 
